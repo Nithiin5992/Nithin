c17c26b85ad20ce6741ef0bdc50e4b43c2485b25
Nithiin5992 <nithinkumar2513@gmail.com>
2023-01-23 19:19:58 +0530

    enable edit

--- .vscode/Nithin.js/dom.js
+++ .vscode/Nithin.js/dom.js
@@ -6,7 +6,7 @@ var Gr = document.getElementById('Graduation').value;
 var yr = document.getElementById('Year of pass').value;
 var li=document.createElement('li');
 li.className="details";
-li.appendChild(document.createTextNode(user+age+Gr+yr));
+li.appendChild(document.createTextNode(user+'-'+age+','+Gr+','+yr));
 abc.appendChild(li);
 console.log(li);
 var deleteBtn = document.createElement('button');
@@ -14,14 +14,22 @@ deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 deleteBtn.appendChild(document.createTextNode('delete'));
 li.appendChild(deleteBtn);
 abc.appendChild(li);
+var EditBtn = document.createElement('button');
+EditBtn.className = 'btn btn-danger btn-sm float-right delete';
+    EditBtn.appendChild(document.createTextNode('Edit'));
+    li.appendChild(EditBtn);
+    abc.appendChild(li);
+    localStorage.setItem("data",user+'-'+age+','+Gr+','+yr)
 }
 abc.addEventListener('click', removeItem);
 function removeItem(e){
   if(e.target.classList.contains('delete')){
     if(confirm('Are You Sure?')){
       var li = e.target.parentElement;
+      
       abc.removeChild(li);
     }
   }
 }
+  
 
