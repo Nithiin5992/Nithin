cb8565dd99c1d8c75bac5dd849cd97ba7f2a776c
Nithiin5992 <nithinkumar2513@gmail.com>
2023-02-08 19:28:20 +0530

    Useful resources

--- .vscode/Nithin.js/dom.js
+++ .vscode/Nithin.js/dom.js
@@ -1,25 +1,25 @@
 var abc =document.getElementById("item");
 function test(){
-var user = document.getElementById('username').value;
-var age = document.getElementById('Age').value;
-var Gr = document.getElementById('Graduation').value;
-var yr = document.getElementById('Year of pass').value;
+var a = document.getElementById('choose experiment').value;
+var b = document.getElementById('choose description').value;
+var c = document.getElementById('Choose a category').value;
 var li=document.createElement('li');
 li.className="details";
-li.appendChild(document.createTextNode(user+'-'+age+','+Gr+','+yr));
+li.appendChild(document.createTextNode(a+'-'+b+'-'+c));
 abc.appendChild(li);
 console.log(li);
 var deleteBtn = document.createElement('button');
 deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-deleteBtn.appendChild(document.createTextNode('delete'));
+deleteBtn.appendChild(document.createTextNode('Delete Expence'));
 li.appendChild(deleteBtn);
 abc.appendChild(li);
 var EditBtn = document.createElement('button');
 EditBtn.className = 'btn btn-danger btn-sm float-right delete';
-    EditBtn.appendChild(document.createTextNode('Edit'));
+    EditBtn.appendChild(document.createTextNode('Edit Expence'));
     li.appendChild(EditBtn);
     abc.appendChild(li);
-    localStorage.setItem("data",user+'-'+age+','+Gr+','+yr)
+    localStorage.setItem("data",a+'-'+b+','+c)
+    
 }
 abc.addEventListener('click', removeItem);
 function removeItem(e){
@@ -32,4 +32,3 @@ function removeItem(e){
   }
 }
   
-
