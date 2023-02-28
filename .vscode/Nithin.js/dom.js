c1049ab9046f353615199f5737765a45a5d30dc7
Nithiin5992 <nithinkumar2513@gmail.com>
2023-02-28 11:24:21 +0530

    save details to curdcurd

---
+++ .vscode/Nithin.js/dom.js
@@ -0,0 +1,45 @@
+var abc =document.getElementById("item");
+function test(){
+var user = document.getElementById('username').value;
+var age = document.getElementById('Age').value;
+var Gr = document.getElementById('Graduation').value;
+var yr = document.getElementById('Year of pass').value;
+var li=document.createElement('li');
+li.className="details";
+li.appendChild(document.createTextNode(user+'-'+age+','+Gr+','+yr));
+abc.appendChild(li);
+console.log(li);
+const obj = {
+    user,
+    age,
+    Gr,
+    yr
+}
+axios.post("https://crudcrud.com/api/41872313b2d947bc9079c7c80a8a00cd/Myuser",obj)
+.then((responce)=>{
+    console.log(responce)
+})
+.catch((err) =>{
+    console.log(err)
+})
+var deleteBtn = document.createElement('button');
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+deleteBtn.appendChild(document.createTextNode('delete'));
+li.appendChild(deleteBtn);
+abc.appendChild(li);
+var EditBtn = document.createElement('button');
+EditBtn.className = 'btn btn-danger btn-sm float-right delete';
+    EditBtn.appendChild(document.createTextNode('Edit'));
+    li.appendChild(EditBtn);
+    abc.appendChild(li);
+}
+abc.addEventListener('click', removeItem);
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+
+      abc.removeChild(li);
+    }
+  }
+}
