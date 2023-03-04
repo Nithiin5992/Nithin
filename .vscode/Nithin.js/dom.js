e2f5de3b21aef1222cad5b36c4e9578f19573760
Nithiin5992 <nithinkumar2513@gmail.com>
2023-03-04 08:06:07 +0530

    update user

--- .vscode/Nithin.js/dom.js
+++ .vscode/Nithin.js/dom.js
@@ -1,86 +1,97 @@
-var abc =document.getElementById("item");
-function test(){
-var user = document.getElementById('username').value;
-var age = document.getElementById('Age').value;
-var Gr = document.getElementById('Graduation').value;
-var yr = document.getElementById('Year of pass').value;
-var li=document.createElement('li');
-li.className="details";
-li.appendChild(document.createTextNode(user+'-'+age+','+Gr+','+yr));
-abc.appendChild(li);
-console.log(li);
-const obj = {
-    user,
-    age,
-    Gr,
-    yr
-}
-axios.post("https://crudcrud.com/api/4a6eae82e2c6428aa77406b2b32a5bef/user",obj)
-.then((responce)=>{
-    console.log(responce)
-})
-.catch((err) =>{
-    console.log(err)
-})
-var deleteBtn = document.createElement('button');
-deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-deleteBtn.appendChild(document.createTextNode('delete'));
-li.appendChild(deleteBtn);
-abc.appendChild(li);
-var EditBtn = document.createElement('button');
-EditBtn.className = 'btn btn-danger btn-sm float-right delete';
-    EditBtn.appendChild(document.createTextNode('Edit'));
-    li.appendChild(EditBtn);
-    abc.appendChild(li);
-}
-abc.addEventListener('click', removeItem);
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
 
-      abc.removeChild(li);
+var details=document.getElementById("form");
+function submit()
+{var a=document.getElementById('Name').value
+
+var obj =
+    {
+      a
     }
+axios.post("https://crudcrud.com/api/641ef2333b7d4d56836a91dbe8021506/myuser",obj)
+.then((responce)=>{
+                    showuseronscreen(responce.data)
+                  })
+.catch((err) =>{
+                console.log(err)
+               })
+ window.addEventListener("DOMContentLoaded",()=>{
+        axios.get("https://crudcrud.com/api/641ef2333b7d4d56836a91dbe8021506/myuser")
+        .then((responce)=>{
+            console.log(responce)
+            for(var i=0;i<responce.data.length;i++){
+                showuseronscreen(responce.data[i])
+            }
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+        })     
+        
+                   
+  function showuseronscreen(user)
+  {
+    var li=document.createElement('li')
+    li.className=details;
+    li.appendChild(document.createTextNode(user.a));
+    console.log(li)
+    details.appendChild(li);
+    adddeletebtn(li)
+    addeditbtn(li)
+    details.addEventListener('click', removeuser);
+    details.addEventListener('click', edituser);
+    function removeuser(e)
+    {
+     
+       if(e.target.classList.contains('delete'))
+       {
+        if(confirm('Are You Sure?'))
+          {
+          var li = e.target.parentElement;
+          details.removeChild(li);
+          axios.delete("https://crudcrud.com/api/641ef2333b7d4d56836a91dbe8021506/myuser/"+user._id)
+          .then((responce)=>{
+                               console.log(responce);
+                               
+                            })
+          .catch((err) =>{
+                          console.log(err)
+                         })
+          }
+       
+      }
+    }
+    function edituser(e)
+    {
+     
+       if(e.target.classList.contains('Edit'))
+       {
+        if(confirm('Are You Sure?'))
+          {
+          removeuser(e)
+           
+          }
+       
+      }
+    }
+}
+   
+   
+  function adddeletebtn(li)
+  {
+     var deleteBtn = document.createElement('button');
+     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+     deleteBtn.appendChild(document.createTextNode('delete'));
+     li.appendChild(deleteBtn);
+     details.appendChild(li);
   }
-}
-window.addEventListener("DOMContentLoaded",()=>{
-axios.get("https://crudcrud.com/api/4a6eae82e2c6428aa77406b2b32a5bef/user")
-.then((responce)=>{
-    console.log(responce)
-    for(var i=0;i<responce.data.length;i++){
-        showuseronscreen(responce.data[i])
-    }
-})
-.catch((err) =>{
-    console.log(err)
-})
-})
-function showuseronscreen(user){
-    var li=document.createElement('li');
-    li.className="details";
-    li.appendChild(document.createTextNode(user.user+","+user.age+","+user.Gr+","+user.yr));
-    abc.appendChild(li);
-    var deleteBtn = document.createElement('button');
-deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-deleteBtn.appendChild(document.createTextNode('delete'));
-li.appendChild(deleteBtn);
-abc.appendChild(li);
-
-abc.addEventListener('click', removeItem);
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-
-      abc.removeChild(li);
-      axios.delete("https://crudcrud.com/api/4a6eae82e2c6428aa77406b2b32a5bef/user/user.id")
-.then((responce)=>{
-    console.log(responce)
-})
-.catch((err) =>{
-    console.log(err)
-})
-    }
+  function addeditbtn(li)
+  {
+     var editBtn = document.createElement('button');
+     editBtn.className = 'btn btn-danger btn-sm float-right Edit';
+     editBtn.appendChild(document.createTextNode('Edit'));
+     li.appendChild(editBtn);
+     details.appendChild(li);
   }
+ 
 }
-}
+  
