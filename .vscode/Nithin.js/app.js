e505bb4b69d6b1077d6111817b1cb4e2368b73a4
Nithiin5992 <nithinkumar2513@gmail.com>
2023-04-13 16:09:03 +0530

    adding body parser

--- .vscode/Nithin.js/app.js
+++ .vscode/Nithin.js/app.js
@@ -1,14 +1,22 @@
-const http = require('http');
-const express = require('express')
-const app =express();
-app.use((req,res,next)=>{
-console.log ("hello");
-next();
-})
-app.use((req,res,next)=>{
-    console.log ("hello 55");
-    res.send('<h1>hello</h1>')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-    })
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
 app.listen(3000);
+
