5f3e8556bab32deb1ea4638ca6ad9972779bbb93
Nithiin5992 <nithinkumar2513@gmail.com>
2023-07-06 16:39:23 +0530

    dynamic routes

--- .vscode/Nithin.js/app.js
+++ .vscode/Nithin.js/app.js
@@ -12,16 +12,13 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactRoutes = require('./routes/contact-us');
-const successRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(contactRoutes);
-app.use(successRoutes);
+
 app.use(errorController.get404);
 
 app.listen(3000);
