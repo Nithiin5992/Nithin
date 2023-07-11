684508c1aacd4bde073a9ecb3c1acad23d00192b
Nithiin5992 <nithinkumar2513@gmail.com>
2023-07-12 00:30:57 +0530

    adding sequelize

--- .vscode/Nithin.js/util/database.js
+++ .vscode/Nithin.js/util/database.js
@@ -1,9 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize');
 
-const pool=mysql.createPool({
-host:'localhost',
-user:'root',
-database:'node_complete',
-password:'Nithin5992'
-})
-module.exports=pool.promise()
+const sequelize = new Sequelize('node_complete', 'root', 'Nithin5992', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
