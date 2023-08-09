b6395a64e976cdb527cb990196e2b7bfdb0f38c6
Nithiin5992 <nithinkumar2513@gmail.com>
2023-08-10 01:27:57 +0530

    Making Expense App Production ready

--- .vscode/Booking appointement app/controllers/expence.js
+++ .vscode/Booking appointement app/controllers/expence.js
@@ -1,6 +1,9 @@
 const Expence = require('../models/expence');
+const sequelize = require('../util/database');
+const User = require('../models/user');
 exports.postdailyexpence = async (req, res, next) => {
     try {
+        const t = await sequelize.transaction();
         const expenceamount = req.body.expenceamount;
         const description = req.body.description;
         const category = req.body.category;
@@ -11,14 +14,16 @@ exports.postdailyexpence = async (req, res, next) => {
             description: description,
             category:category,
             userId:userid
-        })
-        req.user.update({
+        },{transaction:t})
+        await req.user.update({
             totalexpence:totalexpence
-        })
+        },{transaction:t})
 
-        res.status(200).json({ dailyexpence: data, message: 'added daily expence' })
+         await t.commit();
+         res.status(200).json({ dailyexpence: data, message: 'added daily expence' })
     }
     catch (err) {
+        t.rollback();
         console.log(err)
     }
 }
@@ -28,8 +33,14 @@ exports.getdailyexpence = async (req, res, next) => {
     res.status(201).json({ dailyexpence: expences })
 
 }
-exports.deletedailyexpence = (req, res, next) => {
-    const id = req.params.id
-   
-    Expence.distroy({ where: { id: id } })
+
+    
+
+exports.deletedailyexpence = async (req, res, next) => {
+    const id = req.params.id;
+    const data=await Expence.findOne({ where: { id: id } })
+     await Expence.destroy({ where: { id: id } })
+    const user=await  User.findOne({where:{id:data.userId}})
+    const totalexpence=parseInt(user.totalexpence)-parseInt(data.expenceamount);
+    await  user.update({totalexpence:totalexpence})
 }
