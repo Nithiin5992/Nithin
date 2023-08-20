6c2d0b7cba08b2ae779c4791b6874e7c869c49ae
Nithiin5992 <nithinkumar2513@gmail.com>
2023-08-20 17:26:12 +0530

    expencetrackerproject ready to deploy

--- .vscode/expence tracker backend/routes/orderroutes.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const router=express.Router();
-const ordercontroller=require("../controller/order")
-router.post('/postorders',ordercontroller.postorder)
-router.get('/getorders',ordercontroller.getorder)
-router.delete('/deleteorders/:id',ordercontroller.deleteorder)
-router.edit('editorders/:id',ordercontroller.editorder)
-module.exports=router;
--- .vscode/expence tracker backend/util/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node_complete', 'root', 'Nithin5992', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
